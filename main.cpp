1935429779a87c2394c2b892c7194933c279147b
FABIO CESAR FIGUEIREDO DE MEDEIROS CHICOUT <fabio.chicout@ufpe.br>
2024-04-10 11:39:41 -0300

    modularizing a method to generate the integer vector

--- main.cpp
+++ main.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
 #include <vector>
-#include <cstdlib> 
+#include <cstdlib>
 #include <ctime>
+
+void sampleIntegerVector(std::vector<int> &array, int size, int min, int max)
+{
+    srand(time(nullptr));
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand() % ((max - min + 1) + min);
+    }
+}
+
+void printVector(){
+    
+}
 
 int main(int argc, char const *argv[])
 {
     std::vector<int> data;
     int vectorInitialSize = 10;
-    int minRange=0, maxRange=100;
+    int minRange = 0, maxRange = 100;
 
     data.resize(vectorInitialSize);
 
-    srand(time(nullptr));
-
-    for (int i = 0; i < vectorInitialSize; i++)
-    {
-        data[i] = rand() % ((maxRange-minRange+1)+minRange) ;
-    }
+    sampleIntegerVector(data, vectorInitialSize, minRange, maxRange);
 
     for (int i = 0; i < vectorInitialSize; i++)
     {
