ff5b02f7132c12ea410f7f99058acc976bcf637b
FABIO CESAR FIGUEIREDO DE MEDEIROS CHICOUT <fabio.chicout@ufpe.br>
2024-04-10 11:28:19 -0300

    limiting random data to a adjustable range

--- main.cpp
+++ main.cpp
@@ -5,21 +5,19 @@ int main(int argc, char const *argv[])
 {
     std::vector<int> data;
     int vectorInitialSize = 10;
+    int minRange=0, maxRange=100;
 
     data.resize(vectorInitialSize);
 
     for (int i = 0; i < vectorInitialSize; i++)
     {
-        data[i] = rand();
+        data[i] = rand() % ((maxRange-minRange+1)+minRange) ;
     }
-    
-    
 
     for (int i = 0; i < vectorInitialSize; i++)
     {
-        std::cout << data[i] << " " <<  std::endl;
+        std::cout << data[i] << " " << std::endl;
     }
-    
 
     return 0;
 }
